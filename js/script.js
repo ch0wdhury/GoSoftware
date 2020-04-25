f1d9d94db9d9e8ed7d2b5a933ffabe31d8ecfd1c
Ashiq Ali Chowdhury <43188066+ch0wdhury@users.noreply.github.com>
2020-04-26 00:21:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+    $("a").on('click', function(event) {
+
+      if (this.hash !== "") {
+ 
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          window.location.hash = hash;
+        });
+      } 
+    });
+
+
+    $('.owl-carousel').owlCarousel({
+      autoplay: true,
+      items:1,
+      animateOut: 'fadeOut',
+      lazyLoad:true,
+      loop:true,
+      margin:10
+    });
+      $('.logo_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+
